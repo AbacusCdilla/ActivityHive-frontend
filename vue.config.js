a634de0446a206ccd2f5a81869390137c419701e
AbacusCdilla <m.sharjeel.zahid@gmail.com>
2025-03-21 05:00:39 +0500

    vue.config Update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/ActivityHive-frontend/' // <-- Yeh repo ka name hai, isko sahi likho!
-    : '/'
-};
+const { defineConfig } = require('@vue/cli-service');
+
+module.exports = defineConfig({
+  publicPath: '/ActivityHive-frontend/', // Set the public path
+  transpileDependencies: true,
+});
